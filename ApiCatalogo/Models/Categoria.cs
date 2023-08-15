8fd3d961682b6566b68b721163b17f191e49f87e
RubTheKid <jadercardoso@tuta.io>
2023-08-15 14:34:18 -0300

    Add project files.

---
+++ ApiCatalogo/Models/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Models;
+
+public class Categoria
+{
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [Required]
+    public string Nome { get; set; }
+
+    
+    public string ImagemUrl { get; set; }
+    public ICollection<Produto> Produtos { get; set; }
+}
---
+++ ApiCatalogo/Models/Produto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiCatalogo.Models;
+
+public class Produto
+{
+    [Key]
+    public int ProdutoId { get; set; }
+
+    [Required]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Descricao { get; set; }
+
+    [Required]
+    public decimal Preco { get; set; }
+
+    public string ImagemUrl { get; set; } 
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+    public Categoria Categoria { get; set; }
+    public int CategoriaId { get; set; }
+
+}
