b03b85a490075be50a2645fe737efd37208578f4
RubTheKid <jadercardoso@tuta.io>
2023-08-17 09:39:26 -0300

    adding validations

--- ApiCatalogo/Models/Produto.cs
+++ ApiCatalogo/Models/Produto.cs
@@ -23,4 +23,24 @@ public class Produto
     public DateTime DataCadastro { get; set; }
     public Categoria Categoria { get; set; }
     public int CategoriaId { get; set; }
+
+
+    //Validações
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (!string.IsNullOrEmpty(this.Nome))
+        {
+            var primeiraLetra = this.Nome[0].ToString();
+            if (primeiraLetra != primeiraLetra.ToUpper())
+            {
+                yield return new ValidationResult("A primeira letra do produto deve ser maiúscula",new[]{ nameof(this.Nome) });
+            }
+        }
+
+        if (this.Estoque <= 0)
+        {
+            yield return new ValidationResult("O estoque deve ser maior que zero", new[] { nameof(this.Estoque) });
+        }
+    }
+
 }
